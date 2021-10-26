22727e819709e2f6970806383260868da536c7be
bpage203 <bepa0041@colorado.edu>
2021-10-25 21:43:39 -0600

    sql start

--- Project/views/Senior Projects/Ski Jump Model/SimpleSkiJump/Stop.m
+++
@@ -1,7 +0,0 @@
-function [value, isterminal, direction] = Stop(t,s)
-global theta
-value = (theta > -10*pi/180);
-isterminal = 1;  % Halt integration 
-direction = 0;   % The zero can be approached from either direction
-end
-
--- Project/views/Senior Projects/Ski Jump Model/Stop.m
+++
@@ -1,6 +0,0 @@
-function [value, isterminal, direction] = Stop(t,s)
-value = (abs(s(6))<0.01)&(s(7)~=0);
-isterminal = 1;  % Halt integration 
-direction = 0;   % The zero can be approached from either direction
-end
-
